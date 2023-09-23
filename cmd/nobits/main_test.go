cb268617acb6c715304d47641ba374ba6fe83aeb
Jingyuan Liang <jingyuanliang@google.com>
2023-09-23 05:23:14 +0000

    TestNobits using bpf_host.o / bpf_lxc.o

--- cmd/nobits/main_test.go
+++ cmd/nobits/main_test.go
@@ -1,10 +1,47 @@
 package main
 
 import (
+	"bytes"
+	"embed"
 	"testing"
 
-	_ "github.com/cilium/ebpf"
+	"github.com/cilium/ebpf"
 )
 
+//go:embed bpf_host.o
+var bpf_host embed.FS
+
+//go:embed bpf_lxc.o
+var bpf_lxc embed.FS
+
 func TestNobits(t *testing.T) {
+	for _, tc := range []struct {
+		name string
+		data *embed.FS
+	}{
+		{
+			name: "bpf_host.o",
+			data: &bpf_host,
+		},
+		{
+			name: "bpf_lxc.o",
+			data: &bpf_lxc,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			data, err := tc.data.ReadFile(tc.name)
+			if err != nil {
+				t.Fatalf("data.ReadFile: %v", err)
+			}
+
+			spec, err := ebpf.LoadCollectionSpecFromReader(bytes.NewReader(data))
+			if err != nil {
+				t.Fatalf("ebpf.LoadCollectionSpec: %v", err)
+			}
+
+			if spec == nil {
+				t.Errorf("ebpf.LoadCollectionSpec returns nil")
+			}
+		})
+	}
 }
